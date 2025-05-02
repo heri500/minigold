9cdbcba494496c6570f80e19280ebc8abb061037
heri500 <heri500@gmail.com>
2025-05-02 09:34:49 +0700

    after installing radix theme and create sub theme name gipos_theme

---
+++ web/sites/default/files/php/twig/68142da1a58f8_menu.html.twig_MW0F8_4zDEnz8diZMRmuNrv8U/VTqxWYEs1jMmt5CEbm_V0udvV3KWoZoXaPh7ujEHxLc.php
@@ -0,0 +1,97 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* themes/custom/gpos_theme/templates/menu/menu.html.twig */
+class __TwigTemplate_e5410373474b1d3426ee1b294c667083 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 21
+        yield from $this->loadTemplate("radix:nav", "themes/custom/gpos_theme/templates/menu/menu.html.twig", 21)->unwrap()->yield($context);
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/custom/gpos_theme/templates/menu/menu.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  44 => 21,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "themes/custom/gpos_theme/templates/menu/menu.html.twig", "C:\\xampp\\htdocs\\minigold\\web\\themes\\custom\\gpos_theme\\templates\\menu\\menu.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["include" => 21];
+        static $filters = [];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['include'],
+                [],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
